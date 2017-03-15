c654803bb565520b4de6c8b6f3bec4702fb796f4
Bryan Hampton <bryan@zeropointfrontiers.com>
2017-03-15 15:53:36 -0500

    Moving all feature parameters to the UtilFeatures file.

--- UtilClassifiers.py
+++ UtilClassifiers.py
@@ -13,26 +13,17 @@ from UtilFeatures import *
 CLASSIFIER_FILE = 'classifier_calibration.p'
 SCALER_FILE     = 'scaler_calibration.p'
 
-color_space = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-orient = 9  # HOG orientations
-pix_per_cell = 8 # HOG pixels per cell
-cell_per_block = 2 # HOG cells per block
-hog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
-spatial_size = (16, 16) # Spatial binning dimensions
-hist_bins = 32    # Number of histogram bins
-spatial_feat = True # Spatial features on or off
-hist_feat = True # Histogram features on or off
-hog_feat = True # HOG features on or off
-
-
 class CarClassifier:
     def __init__(self):
         if os.path.isfile(CLASSIFIER_FILE):
             # load calibration
-            self.svc      = pickle.load(open(CLASSIFIER_FILE, "rb"))
-            self.X_scaler = pickle.load(open(SCALER_FILE, "rb"))
+            self.svc    = pickle.load(open(CLASSIFIER_FILE, "rb"))
+            self.scaler = pickle.load(open(SCALER_FILE, "rb"))
         else:
             self.train()
+    
+    def predict(self, features):
+        return self.svc.predict(features)
     
     def train(self):
         print("Training the classifier")
@@ -46,35 +37,16 @@ class CarClassifier:
             if 'image' in image or 'extra' in image:
                 notcars.append(image)
             else:
-                cars.append(image)
+                cars.append(image)        
         
-        # TODO REMOVE THIS>>>
-        # Reduce the sample size because
-        # The quiz evaluator times out after 13s of CPU time
-        #sample_size = 500
-        #cars = cars[0:sample_size]
-        #notcars = notcars[0:sample_size]
-        # <<< END REMOVE
-        
-        
-        car_features = extract_features(cars, color_space=color_space, 
-                                spatial_size=spatial_size, hist_bins=hist_bins, 
-                                orient=orient, pix_per_cell=pix_per_cell, 
-                                cell_per_block=cell_per_block, 
-                                hog_channel=hog_channel, spatial_feat=spatial_feat, 
-                                hist_feat=hist_feat, hog_feat=hog_feat)
-        notcar_features = extract_features(notcars, color_space=color_space, 
-                                spatial_size=spatial_size, hist_bins=hist_bins, 
-                                orient=orient, pix_per_cell=pix_per_cell, 
-                                cell_per_block=cell_per_block, 
-                                hog_channel=hog_channel, spatial_feat=spatial_feat, 
-                                hist_feat=hist_feat, hog_feat=hog_feat)
+        car_features    = extract_features(cars)
+        notcar_features = extract_features(notcars)
 
         X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
         # Fit a per-column scaler
-        self.X_scaler = StandardScaler().fit(X)
+        self.scaler = StandardScaler().fit(X)
         # Apply the scaler to X
-        scaled_X = self.X_scaler.transform(X)
+        scaled_X = self.scaler.transform(X)
 
         # Define the labels vector
         y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
@@ -100,7 +72,7 @@ class CarClassifier:
         f = open(CLASSIFIER_FILE, "wb")
         pickle.dump(self.svc,  f)
         f = open(SCALER_FILE, "wb")
-        pickle.dump(self.X_scaler,  f)
+        pickle.dump(self.scaler,  f)
 
 
 
--- UtilFeatures.py
+++ UtilFeatures.py
@@ -3,6 +3,20 @@ import numpy as np
 import cv2
 from skimage.feature import hog
 
+# Constants for feature extractions
+
+kcolor_space = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+korient = 9  # HOG orientations
+kpix_per_cell = 8 # HOG pixels per cell
+kcell_per_block = 2 # HOG cells per block
+khog_channel = 'ALL' # Can be 0, 1, 2, or "ALL"
+kspatial_size = (16, 16) # Spatial binning dimensions
+khist_bins = 32    # Number of histogram bins
+kspatial_feat = True # Spatial features on or off
+khist_feat = True # Histogram features on or off
+khog_feat = True # HOG features on or off
+
+
 # Define a function to return HOG features and visualization
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
@@ -44,10 +58,7 @@ def color_hist(img, nbins=32, bins_range=(0, 256)):
 
 # Define a function to extract features from a list of images
 # Have this function call bin_spatial() and color_hist()
-def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9, 
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):
+def extract_features(imgs):
     # Create a list to append feature vectors to
     features = []
     # Iterate through the list of images
@@ -55,42 +66,9 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
         file_features = []
         # Read in each one by one
         image = mpimg.imread(file)
-        # apply color conversion if other than 'RGB'
-        if color_space != 'RGB':
-            if color_space == 'HSV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-            elif color_space == 'LUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
-            elif color_space == 'HLS':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
-            elif color_space == 'YUV':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
-            elif color_space == 'YCrCb':
-                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
-        else: feature_image = np.copy(image)      
-
-        if spatial_feat == True:
-            spatial_features = bin_spatial(feature_image, size=spatial_size)
-            file_features.append(spatial_features)
-        if hist_feat == True:
-            # Apply color_hist()
-            hist_features = color_hist(feature_image, nbins=hist_bins)
-            file_features.append(hist_features)
-        if hog_feat == True:
-        # Call get_hog_features() with vis=False, feature_vec=True
-            if hog_channel == 'ALL':
-                hog_features = []
-                for channel in range(feature_image.shape[2]):
-                    hog_features.append(get_hog_features(feature_image[:,:,channel], 
-                                        orient, pix_per_cell, cell_per_block, 
-                                        vis=False, feature_vec=True))
-                hog_features = np.ravel(hog_features)        
-            else:
-                hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
-                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-            # Append the new feature vector to the features list
-            file_features.append(hog_features)
-        features.append(np.concatenate(file_features))
+        
+        img_features = single_img_features(image)
+        features.append(img_features)
     # Return list of feature vectors
     return features
 
@@ -98,10 +76,17 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
 # Define a function to extract features from a single image window
 # This function is very similar to extract_features()
 # just for a single image rather than list of images
-def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
-                        hist_bins=32, orient=9, 
-                        pix_per_cell=8, cell_per_block=2, hog_channel=0,
-                        spatial_feat=True, hist_feat=True, hog_feat=True):    
+def single_img_features(img,
+                        color_space    = kcolor_space,
+                        spatial_size   = kspatial_size,
+                        hist_bins      = khist_bins,
+                        orient         = korient, 
+                        pix_per_cell   = kpix_per_cell,
+                        cell_per_block = kcell_per_block,
+                        hog_channel    = khog_channel,
+                        spatial_feat   = kspatial_feat,
+                        hist_feat      = khist_feat,
+                        hog_feat       = khog_feat):
     #1) Define an empty list to receive features
     img_features = []
     #2) Apply color conversion if other than 'RGB'
